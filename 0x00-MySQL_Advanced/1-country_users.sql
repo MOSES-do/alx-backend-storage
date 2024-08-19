5b6757614af60b52db03a47cafc8fe729eae8d12
MOSES-do <archer.me24@gmail.com>
2024-08-19 01:44:36 +0000

    Enumerate list

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- Creates a table with unique users.
+-- country column enforces only three countries
+-- if a value is not set it defaults to US
+-- if a value is set outside three allowed countries, defaults to US
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
