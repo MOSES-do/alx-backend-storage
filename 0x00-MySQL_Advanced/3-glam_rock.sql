f15bd3d9205af4d8e097fde21ec68e63c459055d
MOSES-do <archer.me24@gmail.com>
2024-08-19 02:36:39 +0000

    FIND_IN_SET to search comma-separated values for keyword

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,9 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity.
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+-- FIND_IN_SET function is used to check for a keyword within a comma-separated list of strings
+--  in a given column, it returns 0 if not found and a num > 0 if found 
+-- IFNULL searched column is NULL, IFNULL treats it as an empty string to avoid errors
+-- IF FIND_IN_SET returns a value greater than 0, keyword was found
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
